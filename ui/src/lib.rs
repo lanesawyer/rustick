03a15c9fb60befb842e60cd35b7be0727b36aa3e
Lane Sawyer <github@lanesawyer.dev>
2020-06-22 22:23:16 -0700

    Add basic yew frontend

---
+++ ui/src/lib.rs
@@ -0,0 +1,62 @@
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+struct Model {
+    link: ComponentLink<Self>,
+    value: i64,
+}
+
+enum Msg {
+    AddOne,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+    fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
+        Self {
+            link,
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        match msg {
+            Msg::AddOne => self.value += 1
+        }
+
+        let window: web_sys::Window = web_sys::window().expect("window not available");
+        window.alert_with_message("hello from wasm!").expect("alert failed");
+
+        true
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <div>
+                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+
+    println!("hey");
+    yew::initialize();
+    let document = yew::utils::document();
+    let mount_point = document.query_selector("#mount-point").unwrap().unwrap();
+    println!("{:?}", mount_point);
+
+    App::<Model>::new().mount(mount_point);
+    yew::run_loop();
+}
